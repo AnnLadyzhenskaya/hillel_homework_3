fd321c9993c620ae5b9111f59daa788d99c49f9b
Ann Ladyzhenska <ann.ladyzhenskaya@gmail.com>
2022-11-28 21:23:50 +0100

    homework #5 - triangles

---
+++ homework_5.py
@@ -0,0 +1,68 @@
+"""
+Запросить у пользователя число N - ширина треугольника.
+"""
+
+width = int(input("Enter the expected width: "))
+
+"""
+Вывести треугольник #1 с шириной N с помощью цикла for
+*****
+****
+***
+**
+*
+"""
+
+print("\nFirst triangle")
+for i in range(width, 0, -1):
+    print("*" * i)
+
+"""
+Вывести треугольник #1 с шириной N с помощью цикла for
+*
+**
+***
+****
+*****
+"""
+
+print("\nSecond triangle")
+for i in range(1, width + 1):
+    print("*" * i)
+
+"""
+Вывести треугольник #3 с шириной N с помощью цикла for
+*****
+ ****
+  ***
+   **
+    *
+"""
+
+print("\nThird triangle")
+space = 0
+for i in range(width, 0, -1):
+    print(" " * space + "*" * i)
+    space += 1
+
+"""
+Вывести треугольник #4 с шириной N с помощью цикла for
+    *
+   **
+  ***
+ ****
+*****
+"""
+
+print("\nFourth triangle")
+space = width - 1
+for i in range(1, width + 1):
+    print(" " * space + "*" * i)
+    space -= 1
+
+
+
+
+
+
+
