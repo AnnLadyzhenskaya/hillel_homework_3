96e80ebb39fcb72eb3c2f6c4ddc9cce313fc5672
Ann Ladyzhenska <ann.ladyzhenskaya@gmail.com>
2022-12-01 22:14:27 +0100

    fix with names

--- homework_8.py
+++ homework_8.py
@@ -43,7 +43,7 @@ result_of_requested_access = []
 
 # Проверка имени файла
 def check_file_name(name):
-    if name.find(".") == -1:
+    if name.find(".") == -1 or len(name) < 3 or name.rfind(".") == len(name) - 1 or name.find(".") == 0:
         sys.exit("Invalid file name")
     return True
 
