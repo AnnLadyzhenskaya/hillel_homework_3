5737cbbbeed561c3a70de8ec8324e917a87a8eee
Ann Ladyzhenska <ann.ladyzhenskaya@gmail.com>
2022-12-08 16:46:49 +0100

    task 9 subtask 5-6

--- homework_9.py
+++ homework_9.py
@@ -5,6 +5,7 @@
 """
 import random
 import sys
+import functools
 
 #
 # def find_number(text):
@@ -82,5 +83,42 @@ import sys
 Задание 5
 Дан файл с произвольным текстом, 
 нужно найти количество слов в файле и вывести пользователю
+
+ЧИСЛА ТАКЖЕ СЧИТАЮ ОТДЕЛЬНЫМ СЛОВОМ, так как в ТЗ не указано обратное
 """
 
+# with open("res/txt/random_text.txt", "r") as f:
+#     count = 0
+#     for line in f:
+#         list_line = line.split()
+#         count += len(list_line)
+#
+# print(f"Количество слов: {count}")
+
+
+"""
+Задание 6
+Дан файл в котором записаны числа через пробел, 
+необходимо вывести пользователю сумму этих чисел
+
+Предполагаем, что в файле действительно только числа
+"""
+
+# with open("res/txt/numbers_for_sum.txt", "r") as f:
+#     sum_of_nums = 0
+#     nums = f.readline()
+#     sum_of_nums = functools.reduce(lambda a, b: int(a) + int(b), nums.split())
+#
+# print(f"Сумма чисел из файла numbers_for_sum.txt: {sum_of_nums}")
+
+
+"""
+Задание 7
+
+Дан файл в котором записан текст, необходимо вывести топ 5 строк которые чаще всего повторяются, пример:
+'в' - 20 раз
+'привет' - 10 раз
+'как' - 9 раз
+'у' - 7
+'world' - 4
+"""
