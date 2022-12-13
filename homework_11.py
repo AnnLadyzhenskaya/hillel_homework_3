83728c576a0cb8a5f81505a05b6b3497778b3d74
Ann Ladyzhenska <ann.ladyzhenskaya@gmail.com>
2022-12-13 09:34:35 +0100

    version with closures

--- homework_11.py
+++ homework_11.py
@@ -4,6 +4,7 @@
 f'{func_name} была вызвана {count} раза.\n'
 """
 import fileinput
+import time
 
 
 def call_times(filename):
@@ -28,6 +29,29 @@ def call_times(filename):
     return wrapper
 
 
+def call_times_v2(filename):
+    call_times_v2.counts_dict = {}
+
+    def wrapper(func):
+        def inner(*args, **kwargs):
+            inner.calls_count += 1
+            if filename in call_times_v2.counts_dict.keys():
+                call_times_v2.counts_dict[filename].update({func.__name__: inner.calls_count})
+            else:
+                call_times_v2.counts_dict[filename] = {func.__name__: inner.calls_count}
+
+            for file, func_calls_dict in call_times_v2.counts_dict.items():
+                with open(file, 'w') as f:
+                    for func_name, count in func_calls_dict.items():
+                        f.write(f"{func_name} была вызвана {count} раза.\n")
+
+            return func
+
+        inner.calls_count = 0
+        return inner
+    return wrapper
+
+
 @call_times("res/txt/foo.txt")
 def foo():
     pass
@@ -43,10 +67,42 @@ def doo():
     pass
 
 
+@call_times_v2("res/txt/foo_v2.txt")
+def foo_v2():
+    pass
+
+
+@call_times_v2("res/txt/foo_v2.txt")
+def boo_v2():
+    pass
+
+
+@call_times_v2("res/txt/calls_v2.txt")
+def doo_v2():
+    pass
+
+start = time.time()
+
 foo()
 boo()
 foo()
 foo()
 boo()
 doo()
+
+duration = time.time() - start
+print(f"v1 duration: {duration}")
+
+
+start_v2 = time.time()
+
+foo_v2()
+boo_v2()
+foo_v2()
+foo_v2()
+boo_v2()
+doo_v2()
+
+duration_v2 = time.time() - start_v2
+print(f"v2 duration: {duration_v2}")
 
